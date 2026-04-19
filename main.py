030ed8b40ea7981ff87d7f779bde6a1fa19a76dc
NeonGamer322 <neongamer322@gmail.com>
2026-04-19 11:57:05 +0200

    Fix indent

--- main.py
+++ main.py
@@ -2110,7 +2110,7 @@ class QueueUserView(discord.ui.View):
         if not is_staff_member(member) and queue["opened_by"] != member.id:
             await interaction.response.send_message("Nincs jogod", ephemeral=True)
             return
-            next_player_obj = queue["players"].pop(0)
+        next_player_obj = queue["players"].pop(0)
             queue["called_players"].append(next_player_obj.discord_id)
             await update_queue_message(self.gamemode)
             guild = interaction.guild
